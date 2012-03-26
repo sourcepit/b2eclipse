d22bcfc2acda139efe6c123a091e8bfa72c7c13e
Marco Grupe <marco.grupe@googlemail.com>
2012-03-26 15:42:39 +0200

    Rename2

--- org.sourcepit.b2eclipse/src/org/sourcepit/b2eclipse/ui/WizardPageOne.java
+++ org.sourcepit.b2eclipse/src/org/sourcepit/b2eclipse/ui/WizardPageOne.java
@@ -26,7 +26,6 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
