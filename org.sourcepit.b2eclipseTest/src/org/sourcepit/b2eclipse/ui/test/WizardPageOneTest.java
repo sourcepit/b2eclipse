e7b794fb2fe8d271d326de35b2dcc79af34a39f6
Marco Grupe <marco.grupe@googlemail.com>
2012-03-26 17:05:45 +0200

    First TestCase

---
+++ org.sourcepit.b2eclipseTest/src/org/sourcepit/b2eclipse/ui/test/WizardPageOneTest.java
@@ -0,0 +1,28 @@
+package org.sourcepit.b2eclipse.ui.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class WizardPageOneTest
+{
+
+   @Before
+   public void setUp() throws Exception
+   {
+   }
+
+   @After
+   public void tearDown() throws Exception
+   {
+   }
+
+   @Test
+   public void testCreateControl()
+   {
+      fail("Not yet implemented");
+   }
+
+}
