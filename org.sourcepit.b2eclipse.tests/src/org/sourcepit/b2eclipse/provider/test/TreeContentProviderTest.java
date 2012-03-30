35856aef231a326027ee199232cc965965b783d4
Marco Grupe <marco.grupe@googlemail.com>
2012-03-30 13:14:17 +0200

    - Test failure

--- org.sourcepit.b2eclipse.tests/src/org/sourcepit/b2eclipse/provider/test/TreeContentProviderTest.java
+++ org.sourcepit.b2eclipse.tests/src/org/sourcepit/b2eclipse/provider/test/TreeContentProviderTest.java
@@ -31,11 +31,11 @@ public class TreeContentProviderTest
       
       File[] testProjects = new File[3];
       String userPath =  System.getProperty("user.home");
-      testProjects[0] = new File(userPath +"\\Desktop\\b2-examples-0.1.0-alpha-2\\example-modules\\structured-module\\doc\\org.sourcepit.b2.examples.structured.module.doc\\.project");
-      testProjects[1] = new File(userPath +"\\Desktop\\b2-examples-0.1.0-alpha-2\\example-modules\\structured-module\\plugins\\org.sourcepit.b2.examples.structured.module\\.project");
-      testProjects[2] = new File(userPath +"\\Desktop\\b2-examples-0.1.0-alpha-2\\example-modules\\structured-module\\tests\\org.sourcepit.b2.examples.structured.module.tests\\.project");
+      testProjects[0] = new File(userPath +"\\structured-module\\doc\\org.sourcepit.b2.examples.structured.module.doc\\.project");
+      testProjects[1] = new File(userPath +"\\structured-module\\plugins\\org.sourcepit.b2.examples.structured.module\\.project");
+      testProjects[2] = new File(userPath +"\\structured-module\\tests\\org.sourcepit.b2.examples.structured.module.tests\\.project");
 
-      assertArrayEquals(tcp.getElements(new File("C:\\Users\\imm1199\\Desktop\\b2-examples-0.1.0-alpha-2\\example-modules\\structured-module")),testProjects);
+      assertArrayEquals(tcp.getElements(new File("C:\\Users\\imm1199\\structured-module")),testProjects);
 
       TreeContentProvider.clearArrayList();
 
