97d51bcb578e75990b2c5b7ad8003054a317adce
Marco Grupe <marco.grupe@googlemail.com>
2012-07-31 13:26:29 +0200

    - add new entries to tycho-surefire-plugin configuration -  <useUIHarness>true</useUIHarness> and 	<useUIThread>false</useUIThread> are responsible to run the tests in SWTBot as normal surefire test

---
+++ org.sourcepit.b2eclipse.tests/src/org/sourcepit/b2eclipse/tests/AllContinuousTests.java
@@ -0,0 +1,14 @@
+
+package org.sourcepit.b2eclipse.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+import org.sourcepit.b2eclipse.ui.test.B2WizardTest;
+
+@RunWith(Suite.class)
+@SuiteClasses(B2WizardTest.class)
+public class AllContinuousTests
+{
+
+}
